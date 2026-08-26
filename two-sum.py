a11b3167c9121bbe44fdb2b154469cb8df0a37cf
nathan <nathangabrieldolot@gmail.com>
2026-08-26 17:23:59 +0800

    feat: two problems

---
+++ two-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        hashmap = {}
+
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            x = target - nums[i]
+            if x in hashmap and hashmap[x] != i:
+                return [i, hashmap[x]]
+        return []
