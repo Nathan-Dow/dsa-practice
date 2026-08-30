5730d11a2c03b12f5f63b1b894133c3215bdfbb8
nathan <nathangabrieldolot@gmail.com>
2026-08-30 23:31:42 +0800

    feat: rotate-array

---
+++ array/rotate-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rotate(self, nums: list[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+
+        nums[:] = nums[::-1]
+        nums[:k] = nums[:k][::-1]
+        nums[k:] = nums[k:][::-1]
+
+
+
+
+
+
+                
+        
+        
