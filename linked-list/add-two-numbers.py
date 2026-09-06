90d1af907514dc2f62549f99799edaa923e4dd3f
nathan <nathangabrieldolot@gmail.com>
2026-09-06 22:20:47 +0800

    feat: update add-two-numbers

--- linked-list/add-two-numbers.py
+++ linked-list/add-two-numbers.py
@@ -5,23 +5,28 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-     dummy = ListNode(0)
-     curr = dummy
+     
+     start = ListNode(0)
+     current = start
      carry = 0
 
      while l1 or l2 or carry:
+
         val1 = l1.val if l1 else 0
         val2 = l2.val if l2 else 0
 
-        total = val1 + val2 + carry
-        carry = total // 10
+        result = val1 + val2 + carry
+        carry = result // 10
 
-        curr.next = ListNode(total % 10)
-        curr = curr.next
+        current.next = ListNode(result % 10)
+        current = current.next
 
         if l1:
             l1 = l1.next
         if l2:
             l2 = l2.next
-            
-     return dummy.next
+     
+     return start.next
+     
+     
+    
